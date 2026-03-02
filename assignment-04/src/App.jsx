d53d374bee0e6c0ea5f3a2ac6bbda17bbce32698
Daniel Ramos <ramosdeleon555@umes.edu.gt>
2026-03-01 19:18:17 -0600

    uploading assignment-04

---
+++ assignment-04/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+
+export default function App() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>Mi Aplicación Dockerizada</h1>
+        <p>Proyecto Assignment 04</p>
+        <button>Hola Mundo</button>
+      </div>
+    </div>
+  )
+}
