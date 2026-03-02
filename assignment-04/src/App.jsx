5b9112c026743f5776d9ca443004da865989c612
Daniel Ramos <ramosdeleon555@umes.edu.gt>
2026-03-01 20:02:49 -0600

    updating text version 2

--- assignment-04/src/App.jsx
+++ assignment-04/src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
     <div className="container">
       <div className="card">
         <h1>Mi Aplicación Dockerizada</h1>
-        <p>Proyecto Assignment 04</p>
+        <p>Bienvenido a mi app</p>
         <button>Hola Mundo</button>
       </div>
     </div>
